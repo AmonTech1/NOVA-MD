e5b4a5fd465358ea088aa0f636cbd445595ec7f4
thaghiskbskkns <sparkxtechnologies254@gmail.com>
2026-04-12 15:57:52 +0300

    nova 🐙

---
+++ settings.js
@@ -0,0 +1,6 @@
+// settings.js
+module.exports = {
+    ownerNumber: [''], // Put your number here e.g. ['2547XXXXXXXX']
+    SESSION_ID: "",//Put your session id here 
+    // other settings...
+};
